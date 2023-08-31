abe8ff6610ae9cc40871a08f34a8fa4c1dccce5c
Masoudn <msdnarouei@gmail.com>
2023-08-31 11:53:53 -0500

    Model Preparation Completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifer import logger
 from cnnClassifer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifer.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 #logger.info("Welcome to my custome log")
 
@@ -13,3 +14,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
